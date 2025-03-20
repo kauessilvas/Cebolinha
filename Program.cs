fbd8c33c49cee2f58602a3f22ba4e0183518eae5
kauessilvas <kauesantosds5@gmail.com>
2025-03-20 19:30:29 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+string phrase;
+
+do
+{
+    Console.Write("Digite uma frase: ");
+    phrase = Console.ReadLine()!;
+    if (string.IsNullOrEmpty(phrase))
+    {
+        Console.Write("Por favor digite uma frase!");
+        Thread.Sleep(1000);
+        Console.Clear();
+    }
+    else if (!phrase.Any(char.IsLetter))
+    {
+        Console.Write("Por favor insira apenas letras!");
+        Thread.Sleep(1000);
+        Console.Clear();
+    }
+} while (string.IsNullOrEmpty(phrase) || !phrase.Any(char.IsLetter));
+
+Console.WriteLine($"Cebolinha: {phrase.Replace('r', 'l')}");
